8bc3e12b243620844d557aba32381eed35ee53b2
João Nuno <joaonrb@gmail.com>
2022-12-29 00:53:41 +0100

    Complement test

--- result/Pipe_test.go
+++ result/Pipe_test.go
@@ -1,6 +1,7 @@
 package result_test
 
 import (
+	"github.com/go-errors/errors"
 	. "github.com/joaonrb/alsatian/result"
 	"github.com/stretchr/testify/assert"
 	"strconv"
@@ -25,4 +26,10 @@ func TestPipeOkWithInvalidFunctionShouldReturnErrorResult(t *testing.T) {
 	var ok Result[string] = OK[string]{Value: "J"}
 	result := Pipe(ok, convertInt)
 	assert.IsType(t, Error[int]{}, result)
+}
+
+func TestPipeErrorWithCorrectFunctionShouldReturnErrorResult(t *testing.T) {
+	var err Result[string] = Error[string]{Error: errors.New("something dark side")}
+	result := Pipe(err, convertInt)
+	assert.Equal(t, Error[int]{Error: err.(Error[string]).Error}, result)
 }
